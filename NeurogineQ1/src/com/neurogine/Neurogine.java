55b25be0057768bde8a3d48201a1f5c10e4c76f6
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2023-03-01 15:07:00 +0600

    Done Q1

--- NeurogineQ1/src/com/neurogine/Neurogine.java
+++ NeurogineQ1/src/com/neurogine/Neurogine.java
@@ -1,15 +1,28 @@
 package com.neurogine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Neurogine {
 
-    public String toUpperAndJoin(String[] strArr){
-
-    }
-
     public static void main(String[] args){
-        String[] str= new String[] {"abc","an","","apple","bcd","","jk"};
-        Neurogine neurogine = new Neurogine();
+        String[] strArr= new String[] {"abc","an","","apple","bcd","","jk"};
 
-        
+        String nonEmptyOutput = Arrays.stream(strArr).filter(str-> !str.isEmpty()).map(String::toUpperCase).collect(Collectors.joining(","));
+
+        System.out.println("Convert string to uppercase and join them with comma: " + nonEmptyOutput);
+
+        List<String> stringList = Arrays.stream(strArr).filter(st->st.length()>2).collect(Collectors.toList());
+
+        System.out.println("List with String more than 2 characters: " + stringList);
+
+        long startsWithA = Arrays.stream(strArr).filter(st->st.startsWith("a")).count();
+
+        System.out.println("Number of String which starts with \"a\": " + startsWithA);
+
+        long emptyStr = Arrays.stream(strArr).filter(st->st.isEmpty()).count();
+
+        System.out.println("Count of empty string: " + emptyStr);
     }
 }
