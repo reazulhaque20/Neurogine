540e5b158a9ab372d5d494d8d29650805696412f
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2023-03-01 13:20:18 +0600

    1st commit

---
+++ NeurogineQ1/src/com/neurogine/Neurogine.java
@@ -0,0 +1,15 @@
+package com.neurogine;
+
+public class Neurogine {
+
+    public String toUpperAndJoin(String[] strArr){
+
+    }
+
+    public static void main(String[] args){
+        String[] str= new String[] {"abc","an","","apple","bcd","","jk"};
+        Neurogine neurogine = new Neurogine();
+
+        
+    }
+}
