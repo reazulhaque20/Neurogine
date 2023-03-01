f98dd902bd28056350ad1752805cd22b90981d9e
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2023-03-01 22:43:24 +0600

    done 1

--- NeurogineQ1/src/com/neurogine/Neurogine.java
+++ NeurogineQ1/src/com/neurogine/Neurogine.java
@@ -9,19 +9,28 @@ public class Neurogine {
     public static void main(String[] args){
         String[] strArr= new String[] {"abc","an","","apple","bcd","","jk"};
 
-        String nonEmptyOutput = Arrays.stream(strArr).filter(str-> !str.isEmpty()).map(String::toUpperCase).collect(Collectors.joining(","));
+        String nonEmptyOutput = Arrays.stream(strArr)
+                .filter(str-> !str.isEmpty())
+                .map(String::toUpperCase)
+                .collect(Collectors.joining(","));
 
         System.out.println("Convert string to uppercase and join them with comma: " + nonEmptyOutput);
 
-        List<String> stringList = Arrays.stream(strArr).filter(st->st.length()>2).collect(Collectors.toList());
+        List<String> stringList = Arrays.stream(strArr)
+                .filter(st->st.length()>2)
+                .collect(Collectors.toList());
 
         System.out.println("List with String more than 2 characters: " + stringList);
 
-        long startsWithA = Arrays.stream(strArr).filter(st->st.startsWith("a")).count();
+        long startsWithA = Arrays.stream(strArr)
+                .filter(st->st.startsWith("a"))
+                .count();
 
         System.out.println("Number of String which starts with \"a\": " + startsWithA);
 
-        long emptyStr = Arrays.stream(strArr).filter(st->st.isEmpty()).count();
+        long emptyStr = Arrays.stream(strArr)
+                .filter(String::isEmpty)
+                .count();
 
         System.out.println("Count of empty string: " + emptyStr);
     }
